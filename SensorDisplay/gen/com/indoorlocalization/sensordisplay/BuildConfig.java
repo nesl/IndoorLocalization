7a3e587981fce44c0436c3e63920f40e713f135e
Paul Martin <paul@Pauls-MacBook-Pro-2.local>
2013-06-26 20:26:35 -0700

    added an example sensor app, but with deprecated API calls

---
+++ SensorDisplay/gen/com/indoorlocalization/sensordisplay/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.indoorlocalization.sensordisplay;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
